d960f4aac49dacf5caca3bb257a5869f356ce08b
amritguru <amritguru.work@gmail.com>
2026-01-26 01:04:14 +0530

    solved merge conflicts in TodoContext.jsx

--- src/contexts/TodoContext.jsx
+++ src/contexts/TodoContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = import.meta.env.RENDER_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const TodoContext = createContext();
 
